471c6cd6a982121488922ba7fb67b7c9b2659d5f
martamagui <marta.molina@live.u-tad.com>
2022-01-26 10:49:39 +0100

    Firebase, push up

--- app/src/main/java/com/example/app_demo_chat/MyFirebaseMessagingService.kt
+++ app/src/main/java/com/example/app_demo_chat/MyFirebaseMessagingService.kt
@@ -1,11 +1,38 @@
 package com.example.app_demo_chat
 
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
 import android.util.Log
+import androidx.core.app.NotificationCompat
 import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
 
 class MyFirebaseMessagingService: FirebaseMessagingService(){
     override fun onNewToken(token: String) {
         super.onNewToken(token)
-        Log.d("FCM","token $token")
+        Log.e("FCMMMMMMMMMMMMMMMMMM", "token: $token")
+    }
+
+    override fun onMessageReceived(remoteMessage: RemoteMessage) {
+        super.onMessageReceived(remoteMessage)
+        Log.e("FCM", "notification received")
+        val data = remoteMessage.data
+        val score = data.get("score")
+        val time = data.get("time")
+        val intent = Intent(this, MainActivity::class.java)
+
+        val pendingIntent: PendingIntent = PendingIntent.getActivity(this, 0, intent, 0)
+        val notification = NotificationCompat.Builder(this, "Canal1")
+            .setSmallIcon(R.drawable.ic_launcher_foreground)
+            .setContentTitle(score)
+            .setContentText(time)
+            .setPriority(NotificationCompat.PRIORITY_HIGH)
+            .setContentIntent(pendingIntent)
+            .build()
+        val notificationManager: NotificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        notificationManager.notify(1, notification)
+
     }
 }
